29b540d820a3590f97561d42edd2bec14a93f682
cybermaksx <cybermaksx@proton.me>
2026-05-22 23:22:22 +0400

    20% of the code is ready . Now this script can do TCP scaning against  defaults ports .

--- network_mapper.py
+++ network_mapper.py
@@ -11,7 +11,7 @@ my_ip = my_ip_raw
 port_range = int(input("\n What type of port scanning you would like to do ?  \n 1) Standart Ports for enumerating Standart services \n 2) I want to check all ports on my target \n  "))
 
 
-type_of_scan = int(input("\n What type of scan you want to do : \ n 1) TCP SCAN   \n  2_  SYN Scan  \n Please choose :   "))
+type_of_scan = int(input("\n What type of scan you want to do : \n 1) TCP SCAN\n  2) SYN Scan  \n Please choose :   "))
 
 if port_range == 1:
     ports = [21, 22, 80, 443] #I will add all default ports here 
@@ -21,7 +21,7 @@ elif port_range == 2:
 def tcp_scan (target_ip,target_port ):
     try: 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        settimeout(3)
+        s.settimeout(3)
         s.connect((target_ip,target_port))
         
         print (f"[*] {target_port}  is  open ")
@@ -32,11 +32,6 @@ def tcp_scan (target_ip,target_port ):
         print (f" {target_port} seems to be closed | filtered")
 
 
-
-
-
-
-
-
-
+for port in ports:
+    tcp_scan(target_ip, port)
 
