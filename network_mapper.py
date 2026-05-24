9ebe95df192f4fd7abb74706c99835902396b153
cybermaksx <cybermaksx@proton.me>
2026-05-24 23:44:09 +0400

    added syn scan and check sum funtctions into my network scanner

--- network_mapper.py
+++ network_mapper.py
@@ -35,3 +35,76 @@ def tcp_scan (target_ip,target_port ):
 for port in ports:
     tcp_scan(target_ip, port)
 
+
+def calculate_checksum(data):
+    # If the number of bytes is odd, add a zero byte at the end to make it even
+    if len(data) % 2 != 0:
+        data += b'\x00'
+    
+    # Start with a sum of zero
+    s = 0
+    
+    # Loop through the data, taking 2 bytes at a time
+    for i in range(0, len(data), 2):
+        # Combine two bytes into one 16-bit number (e.g. 0x01 and 0x02 -> 0x0102)
+        word = (data[i] << 8) + data[i + 1]
+        # Add that 16-bit number to the total sum
+        s += word
+    
+    # If the sum is larger than 16 bits, take the overflow and add it back to the lower 16 bits
+    s = (s >> 16) + (s & 0xFFFF)
+    # Do it one more time in case there is still overflow after the first fold
+    s += (s >> 16)
+    
+    # Flip all the bits (ones become zeros, zeros become ones) and keep only 16 bits
+    return ~s & 0xFFFF
+
+
+def syn_scan(target_ip,target_port):
+    source_port = 1234 # our source port
+    #TCP headers fileds
+    seq = 0 #In the beggining seq should be 0 
+    ack = 0 #If seq = 0 ,ack = 0 as well
+    offset_flags = (5 << 12) | 0x002 # size of our packet + SYN flag
+    window = 0  # We are just scanning we don't need to accept anything
+    checksum = 0 # we will use def for this later
+    source_ip = socket.inet_aton(my_ip) #intet_aton converts our strings and ints to bytes
+    dest_ip = socket.inet_aton(ip)  
+
+    tcp_header = struct.pack("!HHLLHHHH", source_port, port, seq, ack, offset_flags, window, checksum, urgent)#converting to the big indian so the server can understand
+
+    #Ip header fields
+    ihl_version = 69  #Internet Header Length which is standart for each ip packet 4 because ipv 4 and 5 because we are using 5 blocks each 4 bytes which gives as 20 and 0100 0101  =  69 
+    tos = 0 #Type Of service or priority  
+    total_length = 40  # 20 bytes ip + 20 bytes tcp
+    identification = 0 #ID of packet we need if it gets damaged however it will not because it's too small 
+    frag_ofset = 0  # we will need this value only if our packet cuttet into pieces
+    ttl = 64 # how many routers we can get trough
+    protocol = 6 # tcp = 6 
+    checksum = 0 
+    source_ip = socket.inet_aton(my_ip)
+    dest_ip = socket.inet_aton(ip)
+    ip_header = struct.pack("!BBHHHBBH4s4s", ihl_version, tos, total_length, identification, frag_offset, ttl, protocol, checksum, source_ip, dest_ip)
+
+    packet = tcp_header + ip_header 
+    
+
+    
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW , socket.IPPROTO_TCP) # SOCKRAW is important here because we don't want out system to touch this socket
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1) # Socket options : DON"T touch socket which we made
+        s.settimeout(3) # Don't wait if answer didn't come in 3 seconds
+        s.sendto(packet, (ip, 0))
+        response = s.recvfrom(1024) #Max response length in bytes
+        tcp = response[0][20:40] # We are getting TCP header from response
+        tcp_fields = struct.unpack("!HHLLHHHH", tcp) # Taking raw bytes back to the human readeble format 
+        
+    
+    
+    
+
+
+
+
+
+
