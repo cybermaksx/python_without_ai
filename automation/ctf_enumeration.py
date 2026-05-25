7b66d869640c2a931c47c97d4d6e8bf8c7b5300a
cybermaksx <cybermaksx@proton.me>
2026-05-25 17:05:05 +0400

    I wrote first part of fast_enum func . Now it can find open ports running nmap and it seems to work ,however,i didn't check it yet . The next step is realising how to learn script undersranding where is web service so it can brute force directories using ffuf

--- automation/ctf_enumeration.py
+++ automation/ctf_enumeration.py
@@ -1,37 +1,35 @@
 import os
-
+import subprocess
 
 target_ip = input("What is your target's ip  : \n")
 
-level_of_enumeration = int(input("What kind of enumeration you want to do ?\n1)Fast enumeration \n 2)Deep enumeration  " ))
-
-
-
-
-
-
-
+level_of_enumeration = int(input("What kind of enumeration you want to do ?\n1)Fast enumeration \n2)Deep enumeration\n  " ))
 
 
 def fast_enumeration (target_ip):
-    pass
+    try:
+        print(f"[*] Running nmap scan against {target_ip} ")
+        subprocess.run(f"sudo nmap -sS -sV -sC -F {target_ip} -oN nmap.txt", shell=True)
+
+        ports = []
 
 
+        with open(f"{options.ip}/nmap_portscan.txt") as f:
+                for line in f:
+                    if "open" in line:
+                        port = line.split("/")[0]
+                        ports.append(port)          
+        
+            ports = ",".join(ports)
+            print("[+] Open ports:", ports)
+    
 
-
-
-
-
-
-
+    
+    
 
 
 def deep_enumeration(target_ip):
     pass
-
-
-
-
 
 
 
