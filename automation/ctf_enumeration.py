a148d4c7f6563a552d98474d35bd989c986ca9e2
cybermaksx <cybermaksx@proton.me>
2026-05-27 02:32:53 +0400

    Now the first Ver is finished . This script can automate nmap scan and dirsearch against specific target . However it's weakness it's that it identify service by looking at ports

--- automation/ctf_enumeration.py
+++ automation/ctf_enumeration.py
@@ -67,11 +67,32 @@ def fast_scan(target_ip):
 
 
 def deep_scan(target_ip):
-    pass
+    try:
+        print(f"[*] Running nmap scan against {target_ip}")
+        subprocess.run(f"sudo nmap -sS -sC -sV {target_ip} -p-  -T3 -oN nmap.txt" , shell=True)
+
+        ports = []
+        with open("nmap.txt") as f:
+            for line in f:
+                if "open" in line:
+                    port = line.split("/")
+                    ports.append(port)
+        ports = ",".join(port)
+        print("[+] Open ports :" , ports)
+
+        if not ports:
+           print("No open ports has been found "
 
 
+        except Exception as e:
+            print(f"Error {e}"
 
 
+        if "80" in ports:
+            target_url = f"http://{target}"
+            web_enumeration(target_url)
+        else:
+            print("No web services")
 
 
 
