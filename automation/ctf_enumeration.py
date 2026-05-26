8159e3269dcb7a9c9e6f110bab82c6628c54fd01
cybermaksx <cybermaksx@proton.me>
2026-05-27 01:16:15 +0400

    Added web_enum func . Still figuruing how to connect all of this the right way

--- automation/ctf_enumeration.py
+++ automation/ctf_enumeration.py
@@ -1,12 +1,44 @@
 import os
 import subprocess
+import json
 
 target_ip = input("What is your target's ip  : \n")
 
 level_of_enumeration = int(input("What kind of enumeration you want to do ?\n1)Fast enumeration \n2)Deep enumeration\n  " ))
 
 
-def fast_enumeration(target_ip):
+def web_enumeration(target_url):
+    output_file = "dirsearch_results.json"
+    try:
+        print(f"Running dirsearch against {target_url}")
+        subprocess.run(f"dirsearch -u {target_url} --format=json -o {output_file}",shell=True check=True) #check = True that python will interpret mistake as exception if one occurs
+        with open(output_file, "r") as f:
+                    data = json.load(f)
+
+
+        directories = []
+
+        for entry in data.get("results", []):
+                    directories.append({
+                        "url": entry.get("url"),
+                        "status": entry.get("status"),
+                        "size": entry.get("content-length"),
+                    })
+
+
+
+        return directories
+
+    except subprocess.CalledProcessError as e:
+        print(f"dirsearch doesn't work {e} ")
+
+    
+
+
+
+
+
+def fast_scan(target_ip):
     try:
         print(f"[*] Running nmap scan against {target_ip}")
         subprocess.run(f"sudo nmap -sS -sV -sC -F {target_ip} -oN nmap.txt", shell=True)
@@ -29,8 +61,11 @@ def fast_enumeration(target_ip):
                              
 
 
-def deep_enumeration(target_ip):
+def deep_scan(target_ip):
     pass
+
+
+
 
 
 
