3e9b1689885b36d4fe1b1cb0af0334062b412225
cybermaksx <cybermaksx@proton.me>
2026-05-26 13:10:07 +0400

    Now it can launch nmap scan ,putting all ports into list and also creates nmap.txt just in case

--- automation/ctf_enumeration.py
+++ automation/ctf_enumeration.py
@@ -6,26 +6,27 @@ target_ip = input("What is your target's ip  : \n")
 level_of_enumeration = int(input("What kind of enumeration you want to do ?\n1)Fast enumeration \n2)Deep enumeration\n  " ))
 
 
-def fast_enumeration (target_ip):
+def fast_enumeration(target_ip):
     try:
-        print(f"[*] Running nmap scan against {target_ip} ")
+        print(f"[*] Running nmap scan against {target_ip}")
         subprocess.run(f"sudo nmap -sS -sV -sC -F {target_ip} -oN nmap.txt", shell=True)
-
-        ports = []
-
-
-        with open(f"{options.ip}/nmap_portscan.txt") as f:
-                for line in f:
-                    if "open" in line:
-                        port = line.split("/")[0]
-                        ports.append(port)          
         
-            ports = ",".join(ports)
-            print("[+] Open ports:", ports)
+        ports = []
+        with open("nmap.txt") as f:
+            for line in f:
+                if "open" in line:
+                    port = line.split("/")[0]
+                    ports.append(port)
+        
+        ports = ",".join(ports)
+        print("[+] Open ports:", ports)
+        
+        if not ports:
+            print("No open ports has been found")
     
-
-    
-    
+    except Exception as e:
+        print(f"Error: {e}")
+                             
 
 
 def deep_enumeration(target_ip):
